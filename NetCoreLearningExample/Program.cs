a601cee9ba64c93f3c6828f320a5ae72451c1c6f
Meow <cat@meowy.org>
2022-03-15 13:38:22 +0800

    feat: Initial commit

---
+++ NetCoreLearningExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreLearningExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
